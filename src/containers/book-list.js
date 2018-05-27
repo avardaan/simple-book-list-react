a4e540e3d4ba5cbbe01122e661df2bd2a50b1c30
Vardaan <vardaan119@gmail.com>
2018-05-26 22:49:45 -0400

    implemented BooksReducer

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class BookList extends Component {
+class BookList extends Component {
 
   renderList() {
     return this.props.books.map(book => {
@@ -18,3 +19,16 @@ export default class BookList extends Component {
     )
   }
 }
+
+// function to connect react to redux. this component is a container.
+function mapStateToProps(state) {
+  // whatever gets returned here shows up as props inside of the current component
+  return {
+    // books key has value of books from redux state
+    books: state.books
+  }
+}
+
+// export container not component
+// apply mapStateToProps to the component and the result is a container with react and redux connected
+export default connect(mapStateToProps)(BookList)
